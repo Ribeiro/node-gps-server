f223a586a56fa63f7f51963c605ed361b90919da
Edward Look <edwlook@gmail.com>
2014-03-06 01:05:37 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Node.js GPS SMS Server
 
+Originally pushed on 7/16/2013
+
 I made this so my little Android app could POST with GPS location info, and then the server would send a text containing the info (parsed in the form of a google maps link) to a chosen phone number via Google Voice.
--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
-	voicejs = require('voice.js'),
-	http = require('http');
+    voicejs = require('voice.js'),
+    http = require('http'),
+    app = express();
 
-var app = express();
-
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.post('/', function(req, res) {
-	var client = new voicejs.Client({
-	email: process.argv[2] || 'EMAIL', //google voice credentials
-	password: process.argv[3] || 'PASS',
-	//tokens: require('./tokens.json')
-	});
+  var client = new voicejs.Client({
+  email: process.argv[2] || 'EMAIL', //google voice credentials
+  password: process.argv[3] || 'PASS',
+  });
 
+  var text = process.argv[4] || 'https://maps.google.com/maps?q=' + req.body.latitude + '+' + req.body.longitude;
+  var to = process.argv.slice(5).length ?  process.argv.slice(5) : [req.body.number];
 
-	var text = process.argv[4] || 'https://maps.google.com/maps?q=' + req.body.latitude + '+' + req.body.longitude;
-	var to = process.argv.slice(5).length ?  process.argv.slice(5) : [req.body.number];
-
-
-	// There are two ways to send texts. 
-
-	// The first method returns the new conversation id, but doesn't allow sending to multiple recipients
-	client.sms({ to: to[0], text: text}, function(err, res, data){
-		if(err){
-			return console.log(err);
-		}
-		console.log('SMS "' +text+ '" sent to', to[0] + '. Conversation id: ', data.send_sms_response.conversation_id);
-	});
+  client.sms({ to: to[0], text: text}, function(err, res, data){
+    if (err) return console.log(err);
+    console.log('SMS "' + text + '" sent to', to[0] + '. Conversation id: ', data.send_sms_response.conversation_id);
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
